2f21e1c5767a2d917c5ff8626b76d991718897e0
B Kleinen <kleinen@infrastructure.de>
2018-07-03 13:34:21 +0200

    explicit constructor and explicit superclass constructor call yielding same error

---
+++ network-refactor-in-class/VideoPost.java
@@ -0,0 +1,10 @@
+
+public class VideoPost extends Post
+{
+    public VideoPost(){
+        super();
+    }
+    public String displaySpecificStuff(){
+        return "video would be shown here";
+    }
+}
