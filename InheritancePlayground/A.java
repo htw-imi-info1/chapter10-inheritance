e5e49a6618faec1c3a50bc596bde764fc88120fb
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-12-19 00:16:36 +0100

    add template method example

--- InheritancePlayground/A.java
+++ InheritancePlayground/A.java
@@ -8,4 +8,12 @@ public class A
     public String nameChain(){
         return "A";
     }
+
+    public String template(){
+        String result = "getName: ";
+        result += getName();
+        result +=" - nameChain: ";
+        result += nameChain();
+        return result;
+    }
 }
